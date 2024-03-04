9b9e065fb37f9325edbf9c444bebf9376515740a
jelena-byrne <jelena.byrne@gmail.com>
2024-03-03 21:04:25 -0600

    committing an intro app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// this file allows us to link styles and templates with typeScript logic
+
+// decorator that identifies as an Angular component
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
+  // application component linked by templateUrl property
   templateUrl: './app.component.html',
+  // application component style file
   styleUrl: './app.component.css'
 })
+
+// typeScript class
 export class AppComponent {
-  title = 'angular-course';
+
+  data = {
+    randomProperty:'Foos!'
+  };
+
+  // property title
+  title = 'Angular Core Deep Dive';
 }
